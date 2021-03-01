1520e119039bf65ae071f9ed229aab3c67706577
Mosindo <64804043+Mosindo@users.noreply.github.com>
2021-03-01 22:50:32 +0100

    data translation  import done

--- express_l10n/index.js
+++ express_l10n/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
-const exphbs = require('express-handlebars')
+const exphbs = require('express-handlebars');
+const translations = require("./translations");
+console.log(translations); 
+
+// fetch('./translations.json')
+//   .then(response => response.json()) 
+//   .then(result => { console.log(result) });
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -11,7 +17,21 @@ app.get('/', function (req, res) {
 
 app.listen(8000,()=>{console.log('server launch')});
 
-app.get('/:lang?', function (req, res) {
-    res.render('home');
+
+
+app.get('/:lang', (req, res) => {
+    if(req.params.id == "fr"){
+        res.send(translations[0].traductio ,)
+    }
+    else if(req.params.id == "es"){
+        res.send(translations[1].traductio)
+    }
+    else if(req.params.id == "en"){
+        res.send(translations[2].traductio)
+    }
+    else{
+        res.send(res.send())
+    }
+    
 });
 
