7dfe7d7a5e8d9587af07b1244b5442a945707f1b
Mosindo <64804043+Mosindo@users.noreply.github.com>
2021-03-02 12:36:40 +0100

    finish

--- chapitre_01/index.js
+++ chapitre_01/index.js
@@ -1,125 +1,105 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
 const port = 8000;
-const autor=[ 'Lawrence Nowell, UK',
-'William Shakespeare, UK',
-'Charles Dickens, US',
-'Oscar Wilde, UK',]
-const Books =['Beowulf','Hamlet, Othello, Romeo and Juliet, MacBeth','Oliver Twist, A Christmas Carol','The Picture of Dorian Gray, The Importance of Being Earnest']
-   
-const autorsData =[
-    {
-        name:'Lawrence Nowell',
-        nationality :'UK',
-        books:['Beowulf'],
-    },
-    {
-        name:'William Shakespeare',
-        nationality :'UK',
-        books:['Hamlet, Othello, Romeo and Juliet, MacBeth'],
-    },
-    {
-        name:'Charles Dickens',
-        nationality :'US',
-        books:['Oliver Twist, A Christmas Carol'],
-    },
-    {
-        name:'Oscar Wilde',
-        nationality :'UK',
-        books:['The Picture of Dorian Gray, The Importance of Being Earnest'],
-    }
-]
 
+const ListAuthors = [
+  {
+    authors: {
+      name: "Lawrence Nowell",
+      nationality: "UK",
+    },
+    books: {
+      books: "Beowulf",
+    },
+  },
+  {
+    authors: {
+      name: "William Shakespeare",
+      nationality: "UK",
+    },
+    books: {
+      books: "Hamlet, Othello, Romeo and Juliet, MacBeth",
+    },
+  },
+  {
+    authors: {
+      name: "Charles Dickens",
+      nationality: "US",
+    },
+    books: {
+      books: "Oliver Twist, A Christmas Carol",
+    },
+  },
+  {
+    authors: {
+      name: "Oscar Wilde",
+      nationality: "UK",
+    },
+    books: {
+      books: "The Picture of Dorian Gray, The Importance of Being Earnest",
+    },
+  },
+];
 
-
+// console.log(ListAuthors[1].books)
 app.listen(port, () => {
-  console.log('Server started on port: ' + port);
-  console.log(autor.length)
-  
+  console.log(`Serveur lancé, pour y acceder ouvrez http://localhost:${port}`);
+});
+app.get("/", (req, res) => {
+  res.send("Authors API");
 });
 
-app.get('/', (req, res) => {
-    res.send('Autors');
-  });
-
-
-  app.get('/autors/:id', (req, res) => {
-    if(req.params.id == 1){
-        res.send(autor[0])
+app.get("/authors/:id", (req, res) => {
+  switch (req.params.id) {
+    case "1":
+      res.send("Lawrence Nowell, UK");
+      break;
+    case "2":
+      res.send("William Shakespeare, UK");
+      break;
+    case "3":
+      res.send("Charles Dickens, US");
+      break;
+    case "4":
+      res.send("Oscar Wilde, UK");
+      break;
+    default:
+      res.send(`the author with the ID ${req.params.id} does not exist`);
+  }
+});
+app.get("/authors/:id/books", (req, res) => {
+  switch (req.params.id) {
+    case "1":
+      res.send("Beowulf");
+      break;
+    case "2":
+      res.send("Hamlet, Othello, Romeo and Juliet, MacBeth");
+      break;
+    case "3":
+      res.send("Oliver Twist, A Christmas Carol");
+      break;
+    case "4":
+      res.send("The Picture of Dorian Gray, The Importance of Being Earnest");
+      break;
+  }
+});
+app.get("/json/authors/:id", (req, res) => {
+    console.log(req.params.id);
+    if (Number.isInteger(parseInt(req.params.id))) {
+        if (parseInt(req.params.id) < ListAuthors.length) {
+            res.send(ListAuthors[req.params.id - 1].authors);
+        } else {
+            res.send(`the author with the ID ${req.params.id} does not exist`);
+        }
+    } else {
+        res.send("l'id doit etre un entier");
     }
-    else if(req.params.id == 2){
-        res.send(autor[1])
-    }
-    else if(req.params.id == 3){
-        res.send(autor[2])
-    }
-    else if(req.params.id == 4){
-        res.send(autor[3])
-    }
-    else{
-        res.send(res.send(`The Autor with the ID ${req.params.id} does not exist`))
-    }
-    
 });
 
-app.get('/autors/:id/books', (req, res) => {
-    if(req.params.id == 1){
-        res.send(Books[0])
-    }
-    else if(req.params.id == 2){
-        res.send(Books[1])
-    }
-    else if(req.params.id == 3){
-        res.send(Books[2])
-    }
-    else if(req.params.id == 4){
-        res.send(Books[3])
-    }
-    else{
-        res.send(`The book with the ID ${req.params.id} does not exist`)
-    }
-    
+app.get("/json/authors/:id/books", (req, res) => {
+  res.send(ListAuthors[req.params.id - 1].books);
 });
 
-app.get('/json', (req, res) => {
-    res.jsonp({autorsData})
-  });
-
-  app.get('/json/autors/:id', (req, res) => {
-    if(req.params.id == 1){
-        res.jsonp({name:autorsData[0].name, nationality:autorsData[0].nationality})
-    }
-    else if(req.params.id == 2){
-        res.jsonp({name:autorsData[1].name, nationality:autorsData[1].nationality})
-    }
-    else if(req.params.id == 3){
-        res.jsonp({name:autorsData[2].name, nationality:autorsData[2].nationality})
-    }
-    else if(req.params.id == 4){
-        res.jsonp({name:autorsData[3].name, nationality:autorsData[3].nationality})
-    }
-    else{
-        res.jsonp(`The Autor with the ID ${req.params.id} does not exist`)
-    }
-    
-});
-
-app.get('/json/autors/:id/book', (req, res) => {
-    if(req.params.id == 1){
-        res.jsonp({Books:autorsData[0].books})
-    }
-    else if(req.params.id == 2){
-        res.jsonp({Books:autorsData[1].books})
-    }
-    else if(req.params.id == 3){
-        res.jsonp({Books:autorsData[2].books})
-    }
-    else if(req.params.id == 4){
-        res.jsonp({Books:autorsData[3].books})
-    }
-    else{
-        res.jsonp(`The book with the ID ${req.params.id} does not exist`)
-    }
-    
+app.get("*", (req, res) => {
+  res.send("ERROR 404");
 });
--- express_l10n/index.js
+++ express_l10n/index.js
@@ -1,41 +1,27 @@
 const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
-const translations = require('./translations.json')
+const translations = require('./translations.json');
+const port = 8000;
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-    res.render('home');
+    res.render('home',{lang :translations[0].traductio});
 });
 
-app.listen(8000,()=>{console.log('server launch')});
+app.listen(port,()=>{console.log('server launch')});
 
 
 
-app.get('/:lang?', (req, res) => {
-    if(req.params.id == "fr"){
-        res.send('home', {
-            lang:translations[0].traductio
-          });
-    }
-    else if(req.params.id == "es"){
-        res.send('home', {
-            lang: translations[1].traductio
-          });
-    }
-    else if(req.params.id == "en"){
-        console.log(translations[2].traductio);
-        res.send('home', {
-            lang: translations[2].traductio
-          });
-    }
-    else{
-        res.send('home', {
-            lang: 'Error'
-          });
-    }
+app.get('/:lang', (req, res) => {
+    if(req.params.lang == "fr"){
+    res.render('home',{lang :translations[0].traductio})}
+    else if(req.params.lang== "es"){
+        res.render('home',{lang :translations[1].traductio})}
+    else if(req.params.lang== "en"){
+        res.render('home',{lang :translations[2].traductio})};
     
 });
 
