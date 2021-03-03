f3ad02f1ce85f47c1ebdcbbd15ea4df328344407
Mosindo <64804043+Mosindo@users.noreply.github.com>
2021-03-03 16:07:09 +0100

    manque css

--- express_integration/index.js
+++ express_integration/index.js
@@ -3,50 +3,17 @@ const app = express();
 const exphbs = require('express-handlebars');
 const port =3000;
 
-const cuiZine = [
-    {
-        pageName:"/",
-        pageHtml:"index.html"
-    },
-    {
-        pageName:"/contact",
-        pageHtml:"contact.html"
-    },
-    {
-        pageName:"/page2",
-        pageHtml:"page2.html"
-    },
-    {
-        pageName:"/post",
-        pageHtml:"post.html"
-    },
-    {
-        pageName:"/recipe",
-        pageHtml:"recipe.html"
-    },
-    {
-        pageName:"/team-members",
-        pageHtml:"team-members.html"
-    },
-    {
-        pageName:"/team",
-        pageHtml:"team.html"
-    },
-]
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-app.use(express.static('public'));
+app.use(express.static('public/css'));
 app.listen(port ,()=>{
     console.log("ready to code")
 })
 
 app.get('/', function (req, res) {
-    res.redirect(cuiZine.pageHtml[0]);
+    res.render('home');
 });
 
-app.get('/:pageName?', function (req, res) {
-    if(req.params.pageName === cuiZine.pageName){
-    res.redirect(cuiZine.pageHtml)}
-});
+
